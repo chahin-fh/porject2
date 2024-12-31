c51dad048d371926f801fc1f222f80ca160e1620
Fhima C. <chahinfhima@gmail.com>
2024-12-31 21:28:57 +0100

    upt1

--- affichage/index.php
+++ affichage/index.php
@@ -1,51 +1,36 @@
 <?php
 include("../cnx.php");
 $res = mysqli_query($cnx,"SELECT NM from matier");
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     extract($_POST);
     $res1 = mysqli_query($cnx , "SELECT n.exmen,n.cc,n.ds,n.notefinal,n.NF from matier as m , perssone as p , note as n where (p.id = m.id)and(p.classe='$cl')and(p.department = '$BR')and(m.NM = '$NM');");
-    function button1($arr,$cnx,$j,$arp){
-        while($ligne = mysqli_fetch_array($res)){
+        while($ligne = mysqli_fetch_array($res1)){
             echo"
-                <style>
-                    #elem1{
-                        position:relative;
-                        left:10%;
-                        background-color: rgba(255,255,255,0.2);
-                        color: black;
-                    }
-                    #elem2{
-                        color: black;
-                        position:relative;
-                        left:15%;
-                    }
-                    #elem3{
-                        color: black;
-                        position:relative;
-                        left:5%;
-                    }
-                    #elem4{
-                        color: black;
-                        position:relative;
-                        left:9%;
-                    }
-                </style>
-                    <tbody>
+            <main>
+                <table class='styled-table'>
+            <thead>
+                <tr>
+                    <th>Nom</th>
+                    <th>cc</th>
+                    <th>ds</th>
+                    <th>exeman</th>
+                    <th>note final</th>
+                </tr>
+            </thead>
+            <tbody>
                         <tr>
-                            <td id='elem1'>$ligne[0]</td>
-                            <td id='elem2'>$ligne[1]</td>
-                            <td id='elem3'>$ligne[2]</td>
-                            <td id='elem4'>$ligne[3]</td>
-                            <td id='elem4'>$ligne[4]</td>
+                            <td id=''>$ligne[0]</td>
+                            <td id=''>$ligne[1]</td>
+                            <td id=''>$ligne[2]</td>
+                            <td id=''>$ligne[3]</td>
+                            <td id=''>$ligne[4]</td>
                         </tr>
-                    </tbody>";
+                    </tbody>
+        </table>
+        </main>
+                    ";
         }
     }
-        $j = 0;
-    // Loop through inputs and insert only non-empty pairs
-    button1();
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -83,17 +68,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </select>
         <input type="submit">
     </form>
-    <main>
-        <table class='styled-table'>
-            <thead>
-                <tr>
-                    <th>Nom</th>
-                    <th>cc</th>
-                    <th>ds</th>
-                    <th>exeman</th>
-                    <th>note final</th>
-                </tr>
-            </thead>
-        </table>
-    </main>
 </body>
