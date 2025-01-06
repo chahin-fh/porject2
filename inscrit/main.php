90429ec2e30f8052ec7e58aa7729f5caee09bd62
Malek Fhima <malekfhima1@gmail.com>
2025-01-06 10:01:39 +0100

    malek

--- inscrit/main.php
+++ inscrit/main.php
@@ -4,8 +4,11 @@ include("../cnx.php");
 $type=$_POST["type"];
 if ($type == "Professeur"){
     $res = mysqli_query($cnx, "INSERT INTO personnes (typ, nom, email, num_tell, adress, ville, code_p, paye, department, titre, date_deb, pass) VALUES ($type, $name, $email, $phone, $address, $city, $postal_code, $country, $department, $academic_title, $hire_date, $password)");
-}else{
+}else if ($type == "Etudiant"){
     $res = mysqli_query($cnx, "INSERT INTO personnes (typ, nom, email, num_tell, adress, ville, code_p, paye, department, date_nais, niveau_etud, date_inscrit) VALUES ($type, $name, $email, $phone, $address, $city, $postal_code, $country, $department, $date_of_birth, $academic_level, $enrollment_date)");
+
+}else{
+    echo "<script>alert('Donner voutre Type');</script>";
 }
 sleep(1);
 header("location:../connexion/index.html");
