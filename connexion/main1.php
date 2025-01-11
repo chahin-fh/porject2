48f26a5ecc292c6df9f8f9590cfa02221c18d00a
Malek Fhima <malekfhima1@gmail.com>
2025-01-11 16:34:10 +0100

    malek

--- connexion/main1.php
+++ connexion/main1.php
@@ -1,12 +1,9 @@
 <?php
 session_start();
 extract($_POST);
-$cnx = mysqli_connect("127.0.0.1","root","","school");
-function hashPassword($password) {
-    return password_hash($password, PASSWORD_BCRYPT);
-}
-$mp = hashPassword($mps);
-$req = "SELECT nom,pass,id from personnes where(nom = '$nom' and pass = '$mp');" or die("problim L4");
+include("../cnx.php");
+
+$req = "SELECT nom,pass,id from personnes where(nom = '$nom' and pass = '$mps');" or die("problim L4");
 $res = mysqli_query($cnx,$req);
 $nb=mysqli_num_rows($res);
 if($nb<1){
