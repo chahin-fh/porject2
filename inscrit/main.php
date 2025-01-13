ce33d170e2a57cffa33e46a9a3c832e9c9afe2b2
Fhima C. <chahinfhima@gmail.com>
2025-01-13 13:01:29 +0100

    php inscri jwou behi

--- inscrit/main.php
+++ inscrit/main.php
@@ -1,61 +1,33 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    include("../cnx.php");
-    if (!$cnx) {
-        die("Erreur de connexion à la base de données: " . mysqli_connect_error());
+include("../cnx.php");
+extract($_POST);
+$res = mysqli_query($cnx,"SELECT * from personnes where  (num_tell='$phone' and email='$email' and nom='$name');");
+$nb = mysqli_num_rows($res);
+$d = date("Y-m-d");
+if($nb>0){
+    die("enti m3ana deja chef");
+}elseif($type === "Etudiant"){
+    $res = mysqli_query($cnx,"SELECT count(classe) from personnes where department = '$department' and typ = 'Etudiant';");
+    $j = mysqli_fetch_array($res)[0];
+    if($j>0){
+    $c = 1;
+    if($j % 20 === 0 ){
+        $c = $j / 20;
+    }else {
+        $c = floor($j);
     }
-    extract($_POST);
-    if ($type == "professeur")
-        if ($stmt === false) {
-            die("Erreur lors de la préparation de la requête: " . mysqli_error($cnx));
-        }
-        $stmt->bind_param("ssssssssssss", $type, $name, $email, $phone, $address, $city, $postal_code, $country, $department, $academic_title, $hire_date, $password);
-    } elseif ($type == "etudiant") {
-        $date_of_birth = mysqli_real_escape_string($cnx, $_POST["date_of_birth"]);
-        $academic_level = mysqli_real_escape_string($cnx, $_POST["academic_level"]);
-        $enrollment_date = date("Y-m-d");
-        $res = mysqli_query($cnx,"SELECT count(classe) from personnes;");
-        $j = mysqli_fetch_array($res)[0];
-        $c = 1;
-        if($j % 20 === 0 ){
-            $c = $j / 20;
-        }else {
-            $c = floor($j);
-        }
-        $c = (string) $c;
-        $a = (string) $academic_level;
-        $classe = $a . $department . $c;
-        $stmt = $cnx->prepare("INSERT INTO personnes (typ, nom, email, num_tell, adress, ville, code_p, paye, department, date_nais, niveau_etud, date_inscrit,classe) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?);");
-        if ($stmt === false) {
-            die("Erreur lors de la préparation de la requête: " . mysqli_error($cnx));
-        }
-        $stmt->bind_param("ssssssssssss", $type, $name, $email, $phone, $address, $city, $postal_code, $country, $department, $date_of_birth, $academic_level, $enrollment_date ,$classe);
-    } else {
-        echo "<script>alert('Veuillez spécifier votre type');</script>";
-        exit;
-    }
-    if ($stmt->execute()) {
-        if ($type == "professeur") {
-            echo "<script>alert('Professeur ajouté');</script>";
-        } else {
-            echo "<script>alert('Etudiant ajouté');</script>";
-        }
-    } else {
-        echo "Erreur: " . $stmt->error;
-    }
-    sleep(1);
-    header("Location: ../connexion/index.html");
-    $stmt->close();
-    mysqli_close($cnx);
+    $c = (string) $c;
+    $a = (string) $academic_level;
+    $classe = $a . $department . $c;
+}else{
+    $a = (string) $academic_level;
+    $classe = $a . $department . "1";
+}
+$resE = mysqli_query($cnx, "INSERT INTO personnes (typ, nom , pass , email, num_tell, adress, ville, code_p, paye, department, date_nais, niveau_etud, date_inscrit,classe) VALUES ('$type', '$name','$password', '$email', '$phone', '$address', '$city', '$postal_code', '$country', '$department', '$date_of_birth', '$academic_level', '$d' ,'$classe');") or die("l24");
+header("location:../menu/index.html");
+}elseif($type === "Professeur"){
+    $resP = mysqli_query($cnx, "INSERT INTO personnes (typ, nom , pass , email, num_tell, adress, ville, code_p, paye, department,titre,date_deb) VALUES ('$type', '$name','$password' ,  '$email', '$phone', '$address', '$city', '$postal_code', '$country', '$department', '$academic_title', '$d');") or die("l24");
+    header("location:../menu/index.html");
+}
 
-?>
-<?php
-
-$cnx = mysqli_connect("127.0.0.1","root","","ifbreak");
-$req5 = "insert into personne (passe,nom,mail,monnaire) values('$mp','$nom','$email','$flous');" or die("probelm l4") ;
-$res9 = mysqli_query($cnx,$req5) or die("problim ins");
-$nb5 = mysqli_affected_rows($cnx);
-sleep(1);
-header("location:../connexion/index.html");
-mysqli_close($cnx);
 ?>
